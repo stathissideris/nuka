79ce7127d414e7d55219df29e179825f42315b6e
Stathis Sideris <stathis@juxt.pro>
2016-01-31 02:13:34 +0000

    Update to Clojure 1.8

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.palletops/stevedore "0.8.0-beta.7"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :pedantic? :abort
-  
+
   :profiles {:dev {:source-paths ["dev" "examples"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
