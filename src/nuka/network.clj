c37458e43bd0bef7610f8856e02919497588dca2
Stathis Sideris <stathis@pixelated-noise.com>
2019-08-22 16:37:33 +0300

    Changed scp function to just return the command, it no longer executes it

--- src/nuka/network.clj
+++ src/nuka/network.clj
@@ -12,17 +12,21 @@
      (>print p)
      (zero? (exit-code p)))))
 
-(defn- scp-file [[{:keys [user host] :as machine} path]]
-  (if machine
-    (str user "@" host ":" path)
-    path))
+(defn- scp-file [x]
+  (if (string? x)
+    x
+    (let [[{:keys [user host] :as machine} path] x]
+     (if machine
+       (str user "@" host ":" path)
+       path))))
 
 (defn scp [src dest & [options]]
   (println (format "scp from %s to %s" (pr-str src) (pr-str dest)))
-  (let [src (if (vector? src) src [nil src])
-        dest (if (vector? dest) dest [nil dest])
-        [src-machine src-file] src
+  (let [src                      (if (vector? src) src [nil src])
+        dest                     (if (vector? dest) dest [nil dest])
+        [src-machine src-file]   src
         [dest-machine dest-file] dest
-        id-file (if src-machine (:id-file src-machine) (:id-file dest-machine))
-        cmd (run-command (call :scp {:i id-file} options (scp-file src) (scp-file dest)))]
-    (run-command cmd)))
+        id-file                  (if src-machine (:id-file src-machine) (:id-file dest-machine))]
+    (if id-file
+      (call :scp {:i id-file} options (scp-file src) (scp-file dest))
+      (call :scp options (scp-file src) (scp-file dest)))))
